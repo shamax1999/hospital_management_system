9cc867c30d9f6a876d3bb11a345190f8773da2c2
Shashiprabha Medhangani <159247387+shamax1999@users.noreply.github.com>
2024-10-13 10:54:49 +0530

    Delete application directory

--- application/config/profiler.php
+++
@@ -1,6 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-/*
- Profiler Section - This file lets you determine whether or not various sections of Profiler
- data are displayed when the Profiler is enabled.
-*/
-
--- application/views/footer.php
+++
@@ -1,5 +0,0 @@
-    <div style="clear:both;color:black; padding:20px;">
-
-    	<hr /><center>&copy; <?php echo date ('Y'); ?> Medicare Hospital</center>
-
-    </div>
